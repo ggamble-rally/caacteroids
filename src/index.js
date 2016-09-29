82c5394edb0b9c03bf6af68d4aabc0bd050c5718
Gary Gamble <ggamble@rallydev.com>
2016-09-29 14:38:01 -0600

    first

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Caacteroids } from './Caacteroids';
+import style from './style.css';
+
+render(<Caacteroids />, document.getElementById('main'));
